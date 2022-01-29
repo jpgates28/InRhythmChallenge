5ef3a4d7cf78ad6bb2b934cf52d1fd163bbd4d55
jpgates28 <96097854+jpgates28@users.noreply.github.com>
2022-01-28 22:38:01 -0500

    Update PaginationHelper.java
    Added additional validations

--- src/inRhythmProject/PaginationHelper.java
+++ src/inRhythmProject/PaginationHelper.java
@@ -71,11 +71,13 @@ public class PaginationHelper {
 		System.out.println(helper.pageItemCount(0)); // should == 4
 		System.out.println(helper.pageItemCount(1)); // last page - should == 2
 		System.out.println(helper.pageItemCount(2)); // should == -1 since the page is invalid
+		System.out.println(helper.pageItemCount(-2)); // should == -1 since the page is invalid
 
 		// pageIndex takes an item index and returns the page that it belongs on
 		System.out.println(helper.pageIndex(5)); // should == 1 (zero based index)
 		System.out.println(helper.pageIndex(2)); // should == 0
 		System.out.println(helper.pageIndex(20)); // should == -1
 		System.out.println(helper.pageIndex(-10)); // should == -1
+		System.out.println(helper.pageIndex(0)); // should == 0
 	}
 }
